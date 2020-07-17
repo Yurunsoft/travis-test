5cf03dbf671712f4b1fad28fb85419d2f698df07
Yurun <admin@yurunsoft.com>
2020-07-17 20:07:03 +0800

    test

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 $atomic = new \Swoole\Atomic(1);
-var_dump($atomic->get(), $atomic->wakeup(), $atomic->get());
+var_dump($atomic->wakeup(), $atomic->get());
 
 $atomic = new \Swoole\Atomic(0);
-var_dump($atomic->get(), $atomic->wakeup(), $atomic->get());
+var_dump($atomic->wakeup(), $atomic->get());
 
 $atomic = new \Swoole\Atomic(0);
-var_dump($atomic->wait(3), $atomic->get());
+var_dump($atomic->wait(1), $atomic->get());
 
 $atomic = new \Swoole\Atomic(1);
-var_dump($atomic->wait(3), $atomic->get());
+var_dump($atomic->wait(1), $atomic->get());
