80a23f463e2baf39eff498b738e0a4145edfb57a
Yurun <admin@yurunsoft.com>
2020-08-13 17:12:13 +0800

    修复

--- test/include/test_util.h
+++ test/include/test_util.h
@@ -21,23 +21,20 @@ using namespace chinese_util;
 #endif
 
 static string get_dir() {
-#ifdef __APPLE__
-    char path[1024];
-    unsigned size = 1024;
-    _NSGetExecutablePath(path, &size);
-    path[size] = '\0';
-    return path;
-#elif _WIN32
-    char szPath[1024] = {0};
-    GetModuleFileName(NULL, szPath, sizeof(szPath) - 1);
-    return szPath;
-#else
     char current_absolute_path[MAX_SIZE];
+#ifdef __APPLE__
+    unsigned size = 1024;
+    _NSGetExecutablePath(current_absolute_path, &size);
+    current_absolute_path[size] = '\0';
+#elif _WIN32
+    GetModuleFileName(NULL, current_absolute_path, sizeof(current_absolute_path) - 1);
+#else
     //获取当前程序绝对路径
     ssize_t cnt = readlink("/proc/self/exe", current_absolute_path, MAX_SIZE);
     if (cnt < 0 || cnt >= MAX_SIZE) {
         return nullptr;
     }
+#endif
     //获取当前目录绝对路径，即去掉程序名
     ssize_t i;
     for (i = cnt - 1; i >= 0; --i) {
@@ -47,7 +44,6 @@ static string get_dir() {
         }
     }
     return current_absolute_path;
-#endif
 }
 
 static inline Dict* get_dict() {
