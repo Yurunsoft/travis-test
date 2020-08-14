f5bcb2749a7e93e701c7d82a35cb6cfe95b5b745
Yurun <admin@yurunsoft.com>
2020-08-14 17:28:51 +0800

    修复

--- .github/workflows/bin/install-php7.4.sh
+++ .github/workflows/bin/install-php7.4.sh
@@ -16,6 +16,8 @@ php-config --extension-dir
 
 curl -o composer.phar https://getcomposer.org/composer-stable.phar && chmod +x composer.phar && sudo mv -f composer.phar /usr/local/bin/composer && composer -V;
 
-echo "xdebug.var_display_max_children=128\
-xdebug.var_display_max_data=512\
-xdebug.var_display_max_depth=16" >> $(php -r "echo php_ini_loaded_file();")
+phpIniFile=$(php -r "echo php_ini_loaded_file();")
+
+echo "xdebug.var_display_max_children=128" >> $phpIniFile
+echo "xdebug.var_display_max_data=512" >> $phpIniFile
+echo "xdebug.var_display_max_depth=16" >> $phpIniFile
