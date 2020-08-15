c01401c2312cd49154ba6a69baef50faef32e349
Yurun <admin@yurunsoft.com>
2020-08-15 13:52:49 +0800

    修复

--- .github/workflows/bin/install-php7.4-mac.sh
+++ .github/workflows/bin/install-php7.4-mac.sh
@@ -6,6 +6,11 @@ brew link --force --overwrite php@7.4
 php -v
 php -m
 php-config
+
+echo "a:"
 ls $(brew --prefix)/opt/php/lib/
+
+echo "b:"
+ls $(brew --prefix)/opt/php/lib/php/
 
 curl -o composer.phar https://getcomposer.org/composer-stable.phar && chmod +x composer.phar && sudo mv -f composer.phar /usr/local/bin/composer && composer -V;
