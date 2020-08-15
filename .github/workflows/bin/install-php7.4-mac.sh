a21ba299814152bd9e1bd02fc8141cfa423d03f5
Yurun <admin@yurunsoft.com>
2020-08-15 14:01:42 +0800

    test

--- .github/workflows/bin/install-php7.4-mac.sh
+++ .github/workflows/bin/install-php7.4-mac.sh
@@ -7,10 +7,15 @@ php -v
 php -m
 php-config
 
+echo $(brew --prefix)/opt/php/lib/
+
 echo "a:"
 ls $(brew --prefix)/opt/php/lib/
 
 echo "b:"
 ls $(brew --prefix)/opt/php/lib/php/
+
+echo "c:"
+ls $(brew --prefix)/opt/php/lib/httpd/
 
 curl -o composer.phar https://getcomposer.org/composer-stable.phar && chmod +x composer.phar && sudo mv -f composer.phar /usr/local/bin/composer && composer -V;
